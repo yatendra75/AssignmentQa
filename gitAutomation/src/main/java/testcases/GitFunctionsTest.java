ae4836b0c226a6b10eaf162b7db0c71fba2a8e29
yatendra4singh@gmail.com <yatendra4singh@gmail.com>
2020-07-11 22:34:49 +0530

    Updated class

--- gitAutomation/src/main/java/testcases/GitFunctionsTest.java
+++ gitAutomation/src/main/java/testcases/GitFunctionsTest.java
@@ -36,13 +36,14 @@ public class GitFunctionsTest extends TestBase {
 
   @Test(enabled = true, dependsOnMethods = {"verifyLogInUserTest"}, priority = 1)
   public void verifyCreateRepositoryTest() {
-    System.out.println("TC: create Repository test started");
+    System.out.println("");
+    System.out.println("Challenge#1: create Repository test started");
     gitFunctions.createRepository(ConstantsAndValues.REPOSITORYCREATION.REPOSITORY_NAME);
   }
 
   @Test(enabled = true, dependsOnMethods = {"verifyCreateRepositoryTest"}, priority = 2)
   public void VerifyIssueCreationInExistingTest() {
-    System.out.println("TC: Issue Creation in existing Repository test started");
+    System.out.println("Challenge#2.1: Issue Creation in existing Repository test started");
     gitFunctions.NavigateToDashboardAndSearchRepoName(ConstantsAndValues.LOGINDETAILS.USERNAME,
         ConstantsAndValues.SEARCHREPO.REPO_NAME_TO_SEARCH);
     Assert.assertTrue(gitFunctions.issueCreation(ConstantsAndValues.ISSUE.ISSUE_TITLE,
@@ -52,7 +53,7 @@ public class GitFunctionsTest extends TestBase {
   @Test(enabled = true, dependsOnMethods = {"VerifyIssueCreationInExistingTest"}, priority = 3)
   public void verifyCreateNewIssueAndEnterIssueIDinDescription() {
     System.out.println(
-        "TC: create new Issue while mentioning previous issue in description test started");
+        "Challenge#2.2: create new Issue while mentioning previous issue in description test started");
     Assert.assertTrue(gitFunctions.createNewIssueAndEnterIssueIDinDescription(
         ConstantsAndValues.ISSUE.NEW_ISSUE_TITLE, ConstantsAndValues.ISSUE.NEW_ISSUE_BODY));
   }
@@ -60,27 +61,27 @@ public class GitFunctionsTest extends TestBase {
   @Test(enabled = true, dependsOnMethods = {"verifyCreateNewIssueAndEnterIssueIDinDescription"},
       priority = 4)
   public void verifyAddNewCommentTest() {
-    System.out.println("TC: Comment to an issue test started");
+    System.out.println("Challenge#3.1: Comment to an issue test started");
     Assert.assertTrue(gitFunctions.addNewComment(ConstantsAndValues.ISSUE.ADD_NEW_COMMENT));
   }
 
   @Test(enabled = true, dependsOnMethods = {"verifyAddNewCommentTest"}, priority = 4)
   public void verifyAddEmojiTest() {
-    System.out.println("TC: Add emoji test started");
+    System.out.println("Challenge#3.2: Add emoji test started");
     Assert.assertTrue(gitFunctions.addEmojiInRepo(ConstantsAndValues.ISSUE.ADD_EMOJI_VALUE));
   }
 
   @Test(enabled = true, dependsOnMethods = {"verifyAddEmojiTest"}, priority = 5)
   public void verifyLinkIssueTest() {
     System.out.println(
-        "TC: Link issue by mentioning in comments and click and navigate to issue test started");
+        "Challenge#4(1&2): Link issue by mentioning in comments and click and navigate to issue test started");
     gitFunctions.newCommentAndMentionPerivousIssueThenNavigateFromCommentIssue(
         ConstantsAndValues.ISSUE.ADD_NEW_COMMENT);
   }
 
   @Test(enabled = true, dependsOnMethods = {"verifyLogInUserTest"}, priority = 6)
   public void verifyDeleteRepositoryTest() {
-    System.out.println("TC: Delete repository test started");
+    System.out.println("Challenge#5: Delete repository test started");
     Assert.assertTrue(
         gitFunctions.NavigateToDashboardAndSearchRepoName(ConstantsAndValues.LOGINDETAILS.USERNAME,
             ConstantsAndValues.SEARCHREPO.REPO_NAME_TO_SEARCH));
